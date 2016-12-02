0273f1df8b45a172789a989d0739a81cf8b995b6
Unknow <przemek.3294@gmail.com>
2016-12-02 12:51:07 +0100

    V2

--- app/src/main/java/com/example/przemek/beeryouwantv2/MainActivity.java
+++ app/src/main/java/com/example/przemek/beeryouwantv2/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("asdasd");
+        System.out.println("Yes");
+        System.out.println("No");
     }
 }
