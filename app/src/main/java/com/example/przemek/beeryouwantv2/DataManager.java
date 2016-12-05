f065427a335501aff991f2f7dae0b7c527ead9c3
Unknow <przemek.3294@gmail.com>
2016-12-05 20:49:32 +0100

    DataManagerV1

--- app/src/main/java/com/example/przemek/beeryouwantv2/DataManager.java
+++ app/src/main/java/com/example/przemek/beeryouwantv2/DataManager.java
@@ -17,44 +17,38 @@ import java.util.List;
 public interface DataManager {
     ALevel getALevel( int idALevel );
     List<ALevel> getALevels();
-    int saveALevel(ALevel aLevel);
-    boolean deleteALevel( int idALevel);
-    boolean updateALevel (ALevel aLevel);
 
     BMLevel getBMLevel( int idBMLevel );
     List<BMLevel> getBMLevels();
-    int saveBMLevel( BMLevel bmLevel);
-    boolean deleteBMLevel( int idBMLevel);
-    boolean updateBMLevel (BMLevel bmLevel);
 
     Beer getBeer(int idBeer );
     List<Beer> getBeers();
-    int saveBeer( Beer beer);
+    /*int saveBeer( Beer beer);
     boolean deleteBeer(int idBeer);
-    boolean updateBeer (Beer beer);
+    boolean updateBeer (Beer beer);*/
 
     Country getCountry(int idCountry );
     List<Country> getCountries();
-    int saveCountry( Country country);
+   /* int saveCountry( Country country);
     boolean deleteCountry(int idCountry);
-    boolean updateCountry (Country country);
+    boolean updateCountry (Country country);*/
 
     Province getProvince(int idProvince );
     List<Province> getProvinces();
-    int saveProvince( Province province);
+    /*int saveProvince( Province province);
     boolean deleteProvince(int idProvince);
-    boolean updateProvince (Province province);
+    boolean updateProvince (Province province);*/
 
     Style getStyle(int idStyle );
     List<Style> getStyles();
-    int saveStyle( Style style);
+    /*int saveStyle( Style style);
     boolean deleteStyle(int idStyle);
-    boolean updateStyle (Style Style);
+    boolean updateStyle (Style Style);*/
 
     Works getWorks(int idWorks );
     List<Works> getWorkses();
-    int saveWorks( Works works);
+    /*int saveWorks( Works works);
     boolean deleteWorks(int idWorks);
-    boolean updateWorks (Works works);
+    boolean updateWorks (Works works);*/
 
 }
