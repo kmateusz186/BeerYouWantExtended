87cd317bc471d9c49b151d6789a184c4dfcbe0a7
kmateusz186 <kmateusz186@gmail.com>
2016-12-05 11:10:48 +0100

    Klasy Dao

--- app/src/main/java/com/example/przemek/beeryouwantv2/WorksListProvinceActivity.java
+++ app/src/main/java/com/example/przemek/beeryouwantv2/WorksListProvinceActivity.java
@@ -1,19 +1,12 @@
 package com.example.przemek.beeryouwantv2;
 
-import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
-import android.widget.Toast;
 
 public class WorksListProvinceActivity extends AppCompatActivity {
 
