28503f2af31fa74550022469213ce33cb8163efd
Unknow <przemek.3294@gmail.com>
2016-12-05 16:23:32 +0100

    asd

--- app/src/main/java/com/example/przemek/beeryouwantv2/DataManager.java
+++ app/src/main/java/com/example/przemek/beeryouwantv2/DataManager.java
@@ -1,9 +1,18 @@
 package com.example.przemek.beeryouwantv2;
 
+import com.example.przemek.beeryouwantv2.model.ALevel;
+
+import java.util.List;
+
 /**
  * Created by Przemek on 04.12.2016.
  */
 
 public interface DataManager {
+    ALevel getALevel( int idALevel );
+    List<ALevel> getALevels();
+    int saveALevel(ALevel aLevel);
+    boolean deleteALevel( int idALevel);
+    boolean updateALvel (ALevel aLevel);
 
 }
