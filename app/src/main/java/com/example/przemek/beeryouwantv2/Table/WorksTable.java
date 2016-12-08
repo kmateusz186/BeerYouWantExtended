a18400ed1642150ab18ced64b6aa90ecaef8d38f
Unknow <przemek.3294@gmail.com>
2016-12-08 18:49:06 +0100

    End

--- app/src/main/java/com/example/przemek/beeryouwantv2/Table/WorksTable.java
+++ app/src/main/java/com/example/przemek/beeryouwantv2/Table/WorksTable.java
@@ -38,31 +38,31 @@ public class WorksTable {
         db.execSQL(CREATE_WORKS_TABLE);
 
         onInsert(db, new Works(0, "Tyskie Browary Książęce",  R.drawable.tyskie_logo, "Znajduje się w Tychach. Jest cześcią koncernu Kompania Piwowarska SA. "
-                + "Producent marek takich jak Tyskie, Lech, Dębowe Mocne, Żubr, a także Książęce.", null, null, 0, null), 2, 1);
+                + "Producent marek takich jak Tyskie, Lech, Dębowe Mocne, Żubr, a także Książęce.", null, 0, null), 2, 1);
 
         onInsert(db, new Works(0, "Browar w Żywcu",  R.drawable.zywiec_logo, "Browar sięga swoją historią do 1856 roku. Obecnie należy do grupy Heineken. "
-                + "Znany z piw Żywiec APA, Desperados, Żywiec Bock i wiele innych.", null, null, 0, null), 2, 1);
+                + "Znany z piw Żywiec APA, Desperados, Żywiec Bock i wiele innych.", null, 0, null), 2, 1);
 
         onInsert(db, new Works(0, "Browar Węgrzce Wielkie",  R.drawable.wegrzce_wielkie_logo, "Mały browar rzemieślniczy zlokalizowany wśród malowniczych pól podkrakowskiej Wieliczki. "
-                + "Producent piw niefiltrowanych, niepasteryzowanych, refermentowanych - Czarny Wilk, Zielony Wilk, Dymione.", null, null, 0, null), 1, 1);
+                + "Producent piw niefiltrowanych, niepasteryzowanych, refermentowanych - Czarny Wilk, Zielony Wilk, Dymione.", null, 0, null), 1, 1);
 
         onInsert(db, new Works(0, "Pracownia Piwa",  R.drawable.pracownia_piwa_logo, "Zlokalizowany koło Krakowa w Modlniczce. "
-                + "Producenci różnych stylów, między innymi Barley Wine, American Pale Ale, Grodziskie, a także Russian Imperial Stout.", null, null, 0, null), 1, 1);
+                + "Producenci różnych stylów, między innymi Barley Wine, American Pale Ale, Grodziskie, a także Russian Imperial Stout.", null, 0, null), 1, 1);
 
         onInsert(db, new Works(0, "Browar Ciechan",  R.drawable.ciechan_logo, "Browar mieszczący się w Ciechanowie. "
-                + "Aktualnie należy do spółki Browary Regionalne Jakubiak Sp. z o.o.", null, null, 0, null), 3, 1);
+                + "Aktualnie należy do spółki Browary Regionalne Jakubiak Sp. z o.o.", null,  0, null), 3, 1);
 
         onInsert(db, new Works(0, "Trzech Kumpli",  R.drawable.trzech_kumpli_logo, "Tarnowski kontraktowy browar lotny - gdyż nie posiada własnego browaru. "
-                + "Uwielbiają India Pale Ale, w związku z czym tworzą różne wariacje tego stylu.", null, null, 0, null), 1, 1);
+                + "Uwielbiają India Pale Ale, w związku z czym tworzą różne wariacje tego stylu.", null, 0, null), 1, 1);
 
         onInsert(db, new Works(0, "Browar Warka",  R.drawable.browar_warka_logo, "Powstały w 1968 roku. Wybrano Warkę z powodu długoletniej tradycji piwowarskiej tego miasta. "
-                + "Znany z piwa Warka Strong.", null, null, 0, null), 3, 1);
+                + "Znany z piwa Warka Strong.", null, 0, null), 3, 1);
 
         onInsert(db, new Works(0, "Browar Olimp",  R.drawable.olimp_logo, "Browar Olimp to inicjatywa kontraktowa powstała w czerwcu 2013 roku w Toruniu. "
-                + "Stworzyła ją dwójka piwnych fanatyków – Michał i Marcin. Michał Olszewski swoimi pomysłami od lat zmienia obraz rynku piwnego w Toruniu.", null, null, 0, null), 4, 1);
+                + "Stworzyła ją dwójka piwnych fanatyków – Michał i Marcin. Michał Olszewski swoimi pomysłami od lat zmienia obraz rynku piwnego w Toruniu.", null,  0, null), 4, 1);
 
         onInsert(db, new Works(0, "Doctor Brew",  R.drawable.doctor_brew_logo, "Browar kontraktowy stworzony przez dwóch pasjonatów. "
-                + "Piwa chmielone są najnowszymi, a także najciekawszymi odmianami chmielu. Wysoka goryczka i niepowtarzalny aromat piwa.", null, null, 0, null), 5, 1);
+                + "Piwa chmielone są najnowszymi, a także najciekawszymi odmianami chmielu. Wysoka goryczka i niepowtarzalny aromat piwa.", null,  0, null), 5, 1);
 
     }
 
